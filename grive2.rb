d8c806d83f173d71f273a6b5120575f10acf4e24
andropenguin <andropenguin@gmail.com>
2017-12-13 16:16:42 +0900

    first commit

---
+++ grive2.rb
@@ -0,0 +1,25 @@
+HOMEBREW_GRIVE2_VERSION="v0.4.2"
+
+class Grive2 < Formula
+  desc "Google Drive client with support for new Drive REST API and partial sync http://yourcmc.ru/wiki/Grive2"
+  homepage "http://yourcmc.ru/wiki/Grive2"
+  url "https://github.com/vitalif/grive2",
+      :using => :git,
+      :tag => HOMEBREW_GRIVE2_VERSION
+
+  version HOMEBREW_GRIVE2_VERSION
+
+  depends_on "cmake" => :build
+
+  def install
+    mkdir "build" do
+      system "cmake", "-G", "Unix Makefiles", "..", *std_cmake_args
+      system "make"
+      system "make", "install"
+    end
+  end
+  
+  test do
+    system "false"
+  end
+end
