481ecb17b74e2a6c4ba6ad024b54b2f498284841
andropenguin <andropenguin@gmail.com>
2017-12-20 20:57:04 +0900

    add :revision tag and version up to v0.5.1-dev

--- grive2.rb
+++ grive2.rb
@@ -1,11 +1,12 @@
-HOMEBREW_GRIVE2_VERSION="v0.4.2"
+HOMEBREW_GRIVE2_VERSION="v0.5.1-dev"
 
 class Grive2 < Formula
   desc "Google Drive client with support for new Drive REST API and partial sync http://yourcmc.ru/wiki/Grive2"
   homepage "http://yourcmc.ru/wiki/Grive2"
   url "https://github.com/vitalif/grive2",
       :using => :git,
-      :tag => HOMEBREW_GRIVE2_VERSION
+      :tag => HOMEBREW_GRIVE2_VERSION,
+      :revision => "84c57c121e03b070f80e1d8fd66749eead7a4d9e"
 
   version HOMEBREW_GRIVE2_VERSION
 
